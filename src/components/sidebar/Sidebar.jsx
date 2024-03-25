439eabbbd9d1e9147a688c3eab01f38e7430ac7a
Santanu Singha <santanu.singha021@gmail.com>
2024-03-25 21:01:17 +0530

    Add axios dependency and update package.json, add AuthContext and useFetch hooks

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -14,7 +14,6 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
-
 const Sidebar = () => {
   const { dispatch } = useContext(DarkModeContext);
   return (
@@ -39,16 +38,18 @@ const Sidebar = () => {
               <span>Users</span>
             </li>
           </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
+          <Link to="/hotels" style={{ textDecoration: "none" }}>
             <li>
               <StoreIcon className="icon" />
-              <span>Products</span>
+              <span>Hotels</span>
             </li>
           </Link>
-          <li>
-            <CreditCardIcon className="icon" />
-            <span>Orders</span>
-          </li>
+          <Link to="/rooms" style={{ textDecoration: "none" }}>
+            <li>
+              <CreditCardIcon className="icon" />
+              <span>Rooms</span>
+            </li>
+          </Link>
           <li>
             <LocalShippingIcon className="icon" />
             <span>Delivery</span>
@@ -99,5 +100,4 @@ const Sidebar = () => {
     </div>
   );
 };
-
 export default Sidebar;
